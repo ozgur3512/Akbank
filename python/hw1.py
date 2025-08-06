5adb8a69773fcb7bd5228c82c6b797faf03772eb
ozgursefer <seferog@alphabot.app>
2025-08-07 02:37:07 +0300

    Simplify

--- python/hw1.py
+++ python/hw1.py
@@ -14,10 +14,7 @@ def reverse_nested_list(data):
     return data
 
 
-input_data = [[1, "a", ["cat"], 2], [[[3]], "dog"], 4, 5]
-output = flatten_list(input_data)
-print(output)  # [1, 'a', 'cat', 2, 3, 'dog', 4, 5]
+print(flatten_list([[1, "a", ["cat"], 2], [[[3]], "dog"], 4, 5]))
 
-input_data = [[1, 2], [3, 4], [5, 6, 7]]
-output = reverse_nested_list(input_data)
-print(output)
+
+print(reverse_nested_list([[1, 2], [3, 4], [5, 6, 7]]))
